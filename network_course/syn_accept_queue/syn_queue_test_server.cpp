e7f990430556293dec8a61f36b43e01add9f0302
Arthur.Zhang <zhangya_no1@qq.com>
2022-05-25 10:44:29 +0800

    add

---
+++ network_course/syn_accept_queue/syn_queue_test_server.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+#include <cstdio>
+#include <sys/socket.h>
+#include <cstdlib>
+#include <cstring>
+#include <arpa/inet.h>
+
+int main() {
+    std::cout << "server start" << std::endl;
+
+    int listen_fd;
+    if ((listen_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        exit(1);
+    }
+    struct sockaddr_in serv_addr{};
+    bzero(&serv_addr, sizeof(serv_addr));
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(9090);
+
+    if (::bind(listen_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) == -1) {
+        exit(1);
+    }
+
+    // 设置 backlog 为 50
+    if (::listen(listen_fd, 50) == -1) {
+        exit(1);
+    }
+
+    while (true) {
+        int client_fd = ::accept(listen_fd, NULL, 0);
+        if (client_fd < 0) {
+            perror("ERROR: accept()");
+            exit(1);
+        }
+        std::cout << "client_fd: " << client_fd << std::endl;
+    }
+    return 0;
+}
+
+
